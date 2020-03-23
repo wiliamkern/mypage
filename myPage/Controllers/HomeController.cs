78b348dac5c2a8ff1a44b9bdc5a7f9ccb1ca735d
Wiliam Kern Franco <45287472+wiliamkern@users.noreply.github.com>
2020-03-22 21:30:45 -0300

    project

---
+++ myPage/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace myPage.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            ViewBag.identification = "Wiliam Kern Franco - Developer";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
